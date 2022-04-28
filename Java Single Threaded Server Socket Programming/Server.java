8b5f957017947d656a47808ae3e59322fab7f1b4
YUNUS EMRE VURGUN <64273622+yunusemrejr@users.noreply.github.com>
2022-04-28 21:54:55 +0300

    Add files via upload

---
+++ Java Single Threaded Server Socket Programming/Server.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter=null;
+        ServerSocket serverSocket = null;
+
+        serverSocket= new ServerSocket(1234);
+
+        while(true){
+            try{
+                socket=serverSocket.accept();
+
+                inputStreamReader = new InputStreamReader(socket.getInputStream());
+                outputStreamWriter= new OutputStreamWriter(socket.getOutputStream());
+
+                bufferedReader=new BufferedReader(inputStreamReader);
+                bufferedWriter=new BufferedWriter(outputStreamWriter);
+
+                while(true){
+                    String msgFromClient= bufferedReader.readLine();
+                    System.out.println("Client: "+msgFromClient);
+                    bufferedWriter.write("Message Received!");
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+
+                    if(msgFromClient.equalsIgnoreCase("bye"))
+                        break;
+                }
+
+                socket.close();
+                inputStreamReader.close();
+                outputStreamWriter.close();
+                bufferedReader.close();
+                bufferedWriter.close();
+
+
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+}
